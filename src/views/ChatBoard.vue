cb472c3d9ebbaa00b7074b84be600ba1367a7721
nakashi94 <n0904yBUNJOU@gmail.com>
2021-09-14 21:50:51 +0900

    chat

--- src/views/ChatBoard.vue
+++ src/views/ChatBoard.vue
@@ -16,26 +16,23 @@
               <v-subheader>{{ card }}</v-subheader>
 
               <v-list two-line>
-                <template v-for="n in 6">
+                <template v-for="(data, index) in messages">
                   <v-list-item
 
-                    :key="n"
+                    :key="index"
                   >
                     <v-list-item-avatar color="grey darken-1">
                     </v-list-item-avatar>
-
                     <v-list-item-content>
-                      <!-- v-list-item-title>Message {{ n }}</v-list-item-title -->
-
                       <v-list-item-subtitle class="message">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil repellendus distinctio similique
+                        {{ data.message }}
                       </v-list-item-subtitle>
                     </v-list-item-content>
                   </v-list-item>
 
                   <v-divider
                     v-if="n !== 6"
-                    :key="`divider-${n}`"
+                    :key="`divider-${index}`"
                     inset
                   ></v-divider>
                 </template>
@@ -45,12 +42,26 @@
         </v-row>
       </v-container>
       <v-textarea
+          v-model="body"
           append-icon="mdi-comment"
           class="mx-2"
           label="メッセージを送信する"
           rows="3"
           auto-grow
         ></v-textarea>
+
+        <v-btn
+          class="mr-4"
+          type="submit"
+          :disabled="invalid"
+          @click="submit"
+        >
+          submit
+        </v-btn>
+        <v-btn @click="clear">
+          clear
+        </v-btn>
+
     </v-main>
   </v-app>
 </template>
@@ -62,7 +73,15 @@
           console.log("user_id", this.user_id);
       },
     data: () => ({
-        user_id: '',
+      messages: [
+        {message: "messaage 1"},
+        {message: "messaage 2"},
+        {message: "messaage 3"},
+        {message: "messaage 4"},
+        {message: "messaage 5"},
+      ],
+      body: '',
+      user_id: '',
       cards: ['Today'],
       drawer: null,
       links: [
@@ -71,7 +90,28 @@
         ['mdi-delete', 'Trash'],
         ['mdi-alert-octagon', 'Spam'],
       ],
+      // invalid: false
     }),
+    computed: {
+      invalid() {
+        console.log("invalid call.")
+        if (!this.body) {
+          return true;
+        }
+        return false;
+      }
+    },
+    methods: {
+      clear() {
+        console.log("clear call.")
+        this.body = ""
+      },
+      submit() {
+        console.log("submit call.", this.body)
+        this.messages.unshift({message: this.body});
+        this.body = "";
+      },
+    }
   }
 </script>
 
